088c6fdbcfaf056ea49717df94c8e3cf75f3348b
FratiaXd <welcomcherry@gmail.com>
2022-03-31 12:47:13 +0100

    first commit

---
+++ iap2/iap2/Model/AppDataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace iap2.Model
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ iap2/iap2/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace iap2.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Size { get; set; }
+        public string Ingredients { get; set; }
+        public double Price { get; set; }
+        public string ProductImage { get; set; }
+        public string ProductImageAlt { get; set; }
+
+        //more images for single prod page
+    }
+}
