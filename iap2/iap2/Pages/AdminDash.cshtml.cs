f212da7eea9cc3de9d3a9313f7fb422db70fc892
FratiaXd <welcomcherry@gmail.com>
2022-04-02 13:42:12 +0100

    added role manager, add/delete roles

--- iap2/iap2/Pages/AdminDash.cshtml.cs
+++ iap2/iap2/Pages/AdminDash.cshtml.cs
@@ -1,8 +1,11 @@
+using iap2.Model;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace iap2.Pages
 {
@@ -10,14 +13,37 @@ namespace iap2.Pages
     {
         private readonly RoleManager<IdentityRole> _roleManager;
         public List<IdentityRole> roles { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Id { get; set; }
+        [BindProperty]
+        public string roleName { get; set; }
 
         public AdminDashModel(RoleManager<IdentityRole> roleManager)
         {
             _roleManager = roleManager;
         }
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
-            roles = _roleManager.Roles.ToList();
+            roles = await _roleManager.Roles.ToListAsync();
+            return Page();
+        }
+        public async Task<IActionResult> OnGetDelete()
+        {
+            var role = await _roleManager.FindByIdAsync(Id);
+            var result = await _roleManager.DeleteAsync(role);
+            return RedirectToPage("AdminDash");
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (roleName != null)
+            {
+                await _roleManager.CreateAsync(new IdentityRole(roleName.Trim()));
+            }
+            else
+            {
+                ModelState.AddModelError("", "No role specified");
+            }
+            return RedirectToPage("AdminDash");
         }
     }
 }
