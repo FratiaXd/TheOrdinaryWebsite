5151e8a996f932728483ac3a97da953aad1583e1
FratiaXd <welcomcherry@gmail.com>
2022-04-18 16:20:42 +0100

    design finished

--- iap2/iap2/Pages/products.cshtml.cs
+++ iap2/iap2/Pages/products.cshtml.cs
@@ -2,6 +2,7 @@ using iap2.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,13 +15,18 @@ namespace iap2.Pages
         [BindProperty (SupportsGet = true)]
         public string category { get; set; }
         public IEnumerable<Product> itemsList { get; set; }
+        public List<Product> products { get; set; }
         public IEnumerable<string> ProdCategories { get; set; }
 
         [BindProperty (SupportsGet = true)]
         public int Id { get; set; }
+        public Product Recommen1 { get; set; }
+
+        public Product Recommen2 { get; set; }
 
         private readonly AppDataContext _db;
 
+        static Random rnd = new Random();
         public productsModel(AppDataContext db)
         {
             _db = db;
@@ -28,6 +34,18 @@ namespace iap2.Pages
         public void OnGet()
         {
             itemsList = _db.Products.ToList();
+            products = _db.Products.ToList();
+            int r = rnd.Next(products.Count);
+            if (r == products.Count-1)
+            {
+                Recommen1 = products[r];
+                Recommen2 = products[r-1];
+            }
+            else
+            {
+                Recommen1 = products[r];
+                Recommen2 = products[r+1];
+            }           
             ProdCategories = _db.Products.Select(p => p.Category).Distinct();
         }
         public void OnPost()
